5dea843a813e6d1fdc537fa2f68acd0a3f966913
yovelcohen <yovel.cohen@herolo.co.il>
2021-01-22 09:15:12 +0200

    added new sites

--- {{cookiecutter.project_name}}/data/ETLs/Flows/Zoura/Extractor.py
+++ {{cookiecutter.project_name}}/data/ETLs/Flows/Zoura/Extractor.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from api.models import FarmKPIs
+from api.models import FarmScores
 from common.consts import (FARM__ACCOUNT, FARM__ACCOUNT__ID, FARM__NAME, ASSIGNED_TAG_COUNT,
                            FARM__ACCOUNT__BILLING_ID, DATE, FARM)
 from common.dates.consts import TODAY
@@ -13,8 +13,8 @@ class DataForBillingReportExtractor(BaseExtractor):
         CURRENT_MONTH = TODAY.month
         PREV_MONTH = CURRENT_MONTH - 1
         account_ids = self.accounts.values_list('id', flat=True)
-        query = FarmKPIs.objects.filter(farm__account__in=account_ids, date__month=PREV_MONTH, date__day=15,
-                                        date__year=CURRENT_YEAR).select_related(FARM, FARM__ACCOUNT)
+        query = FarmScores.objects.filter(farm__account__in=account_ids, date__month=PREV_MONTH, date__day=15,
+                                          date__year=CURRENT_YEAR).select_related(FARM, FARM__ACCOUNT)
         if not query.exists():
             df = pd.DataFrame()
         else:
